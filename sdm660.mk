2e0810bb0266d8b0ffcabb846ffbefc99278edf1
Tiktodz <ewprjkt@proton.me>
2022-08-15 12:49:01 +0000

    sdm660-common: overlay: lineage => nad
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -281,12 +281,9 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-nad
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Permissions
 PRODUCT_COPY_FILES += \
