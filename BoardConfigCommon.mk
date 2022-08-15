trpsl/android_device_motorola_sdm660-common

sdm660-common: Use generic arch variant
Follow blueline which has not demonstrated any regressions
after switching to a generic arch variant.

Bug: 120773446
Test: make
Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

BoardConfigCommon.mk
@@ -33,7 +33,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := kryo
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm

predator112/device_asus_sdm660-common

BoardConfigCommon.mk
@@ -19,7 +19,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a73
+TARGET_2ND_CPU_VARIANT := generic
 
 # Audio
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true